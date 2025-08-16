5929efde382bf3247aa9d553f1350e8acd842fea
Valentyn Radchuk <zaknafeyn@gmail.com>
2025-08-16 18:14:50 +0200

    fix circular dependency

--- src/webview/hooks/useSendMessage.ts
+++ src/webview/hooks/useSendMessage.ts
@@ -1,22 +1,17 @@
 import { useCallback } from 'react';
-import { useLogger } from './useLogger';
 import { WebviewCommand } from '@/types/commands';
 
 export const useSendMessage = () => {
-  const logger = useLogger(false);
+  const sendMessage = useCallback((command: WebviewCommand, data?: any) => {
+    if (typeof window !== 'undefined' && (window as any).vscode) {
+      console.debug(`[Webview]: Sending command: ${command}`);
 
-  const sendMessage = useCallback(
-    (command: WebviewCommand, data?: any) => {
-      if (typeof window !== 'undefined' && (window as any).vscode) {
-        logger.debug(`Sending command: ${command}`);
-        (window as any).vscode.postMessage({
-          command,
-          ...data,
-        });
-      }
-    },
-    [logger]
-  );
+      (window as any).vscode.postMessage({
+        command,
+        ...data,
+      });
+    }
+  }, []);
 
   return sendMessage;
 };
