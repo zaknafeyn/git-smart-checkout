d94ef769b0776fd3bf80d96671d32ceb45a3487f
Valentyn Radchuk <zaknafeyn@gmail.com>
2025-08-17 11:55:58 +0200

    feat: Refactor GitExecutor to use SimpleGit for improved command execution and add new utility functions; remove unused execCommand and createPromiseWithResolvers utilities

--- src/utils/createPromiseWithResolvers.ts
+++
@@ -1,9 +0,0 @@
-export function createPromiseWithResolvers<T>() {
-  let resolve!: (value: T | PromiseLike<T>) => void;
-  let reject!: (reason?: unknown) => void;
-  const promise = new Promise<T>((res, rej) => {
-    resolve = res;
-    reject = rej;
-  });
-  return { promise, resolve, reject };
-}
