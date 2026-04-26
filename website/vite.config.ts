f1fbd54b471082965acd910c84bc27d6317ecc08
Valentyn Radchuk <zaknafeyn@gmail.com>
2026-04-26 22:59:42 +0200

    Add CNAME file for custom domain and update Vite config base path

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/git-smart-checkout/',
+  base: '/',
+  // base: '/git-smart-checkout/',
 });
